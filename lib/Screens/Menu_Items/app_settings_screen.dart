0a3dccbb2cac055dc519098c475552f1ef6f298d
asmaa azmi <106414071+asmaa23azmi@users.noreply.github.com>
2023-07-10 13:27:33 +0300

    update_03

--- lib/Screens/Menu_Items/app_settings_screen.dart
+++ lib/Screens/Menu_Items/app_settings_screen.dart
@@ -1,9 +1,11 @@
+import 'package:cornorshop/Widgets/my_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../Const/colors.dart';
 import '../../Const/texts.dart';
+import '../../enums.dart';
 
 class AppSettingScreen extends StatefulWidget {
   const AppSettingScreen({super.key});
@@ -13,6 +15,9 @@ class AppSettingScreen extends StatefulWidget {
 }
 
 class _AppSettingScreenState extends State<AppSettingScreen> {
+  late AppLocalizations appLocale = AppLocalizations.of(context)!;
+  String? radioValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +40,90 @@ class _AppSettingScreenState extends State<AppSettingScreen> {
         centerTitle: true,
         title: Text(AppLocalizations.of(context)!.appSettings, style: textAppBarStyle,),
       ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsetsDirectional.symmetric(horizontal: 20.w , vertical: 20.h),
+          physics:const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              MyListTile(onTap: (){
+                return showModalBottomSheet(
+                    context: context,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusDirectional.only(
+                          topStart: Radius.circular(20.r) ,
+                          topEnd: Radius.circular(20.r))),
+                    builder: (context) {
+                      return Padding(
+                        padding: EdgeInsetsDirectional.symmetric(horizontal: 26.w , vertical:10.h ),
+                        child: Column(
+                          children: [
+                            ///Black Divider
+                            Container(
+                              height: 4,
+                              margin: EdgeInsetsDirectional.symmetric(
+                                  horizontal: MediaQuery.of(context).size.width * 0.4,
+                                  vertical: MediaQuery.of(context).size.height * 0.015),
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadiusDirectional.circular(2.r),
+                              ),
+                            ),
+                            SizedBox(height:20.h,),
+                            ///Title
+                            Row(
+                              children: [
+                                InkWell(
+                                  onTap:(){Navigator.pop(context);},
+                                    child: Icon(Icons.close,color: darkBlue,size: 20.w,)),
+                                SizedBox(width: 15.w,),
+                                Text(appLocale.appLanguage, style: TextStyle(fontWeight: FontWeight.bold,
+                                  fontSize: 14.0.sp, color: darkBlue,),),
+                              ],
+                            ),
+                            SizedBox(height:10.h,),
+                            Divider(color: grayColor, height: 0.3.h,),
+                            ///Items
+                            Row(
+                              children: [
+                                Radio(value: AppLanguages.ar.name,
+                                  groupValue: radioValue,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      radioValue = value;
+                                    });
+                                  },),
+                                SizedBox(width: 3.w,),
+                                Text('العربية',
+                                  style: TextStyle(fontSize: 14.sp, color: darkBlue,),),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Radio(value: AppLanguages.en.name,
+                                  groupValue: radioValue,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      radioValue = value;
+                                    });
+                                  },),
+                                SizedBox(width: 3.w,),
+                                Text('English',
+                                  style: TextStyle(fontSize: 14.sp, color: darkBlue,),),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },);
+              },text: appLocale.appLanguage, icon: Icons.language_rounded),
+              SizedBox(height: 12.h,),
+            ],
+          ),
+        )
+      ),
     );
   }
 }
+
+
---
+++ lib/Widgets/my_list_tile.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../Const/colors.dart';
+import '../Const/texts.dart';
+
+class MyListTile extends StatelessWidget {
+  late String text;
+  late IconData? icon;
+  late Function()? onTap;
+   MyListTile({
+    required this.text,
+    required this.icon,
+     this.onTap,
+    super.key,
+
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      onTap: onTap,
+      child: Row(
+        children: [
+          Icon(icon, color: darkBlue, size: 20.w,),
+          SizedBox(width: 25.w,),
+          Text(text, style: textStyle,),
+        ],
+      ),
+    );
+  }
+}
